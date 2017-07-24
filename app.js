90df24acc557769d463afdcc7c98dab3bbcf62f6
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-07-24 10:37:36 -0400

    dokku

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const app = express();
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
