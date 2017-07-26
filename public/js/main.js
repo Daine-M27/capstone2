21baef129bb5f2ed78271161a5b2ee5d415b061c
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-07-26 18:42:13 -0400

    image preview in gallery upload

--- public/js/main.js
+++ public/js/main.js
@@ -84,6 +84,19 @@ $(document).ready(() => {
     //
     // });
 
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
 
 });
 
