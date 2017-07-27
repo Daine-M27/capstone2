550528047202bf865a19043a071a37ea1dc0ea02
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-07-26 20:14:05 -0400

    image preview in gallery upload

--- public/js/main.js
+++ public/js/main.js
@@ -56,7 +56,7 @@ $(document).ready(() => {
         reader.onload = function (e) {
             // get loaded data and render thumbnail.
             document.getElementById("upload-preview").src = e.target.result;
-        })
+        });
 
         // read the image file as a data URL.
         reader.readAsDataURL(this.files[0]);
