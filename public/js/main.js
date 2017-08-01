37d87a11c2d2afc8e834384eb5eb1a1bbe4822de
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-07-31 21:55:38 -0400

    fixed main.js

--- public/js/main.js
+++ public/js/main.js
@@ -18,26 +18,27 @@ jQuery.each( [ "put", "delete" ], function( i, method ) {
 
 
 $(document).ready(() => {
-    Initialize Tooltip
-      $('[data-toggle="tooltip"]').tooltip();
-
-        // Add smooth scrolling to all links in navbar + footer link
-      $(".navbar a.scroll, footer a.scroll[href='#myPage']").on('click', function (event) {
-            // Prevent default anchor click behavior
-        event.preventDefault();
-
-            // Store hash
-        const hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 900, () => {
-                // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      });
+        // <<<<<<< HEAD
+        //     Initialize Tooltip
+        //       $('[data-toggle="tooltip"]').tooltip();
+        //
+        //         // Add smooth scrolling to all links in navbar + footer link
+        //       $(".navbar a.scroll, footer a.scroll[href='#myPage']").on('click', function (event) {
+        //             // Prevent default anchor click behavior
+        //         event.preventDefault();
+        //
+        //             // Store hash
+        //         const hash = this.hash;
+        //
+        //             // Using jQuery's animate() method to add smooth page scroll
+        //             // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        //         $('html, body').animate({
+        //           scrollTop: $(hash).offset().top
+        //         }, 900, () => {
+        //                 // Add hash (#) to URL when done scrolling (default click behavior)
+        //           window.location.hash = hash;
+        //         });
+        //       });
 
 
 
@@ -50,8 +51,9 @@ $(document).ready(() => {
             const reader = new FileReader();
 
             reader.onload = function (e) {
-                $('img.upload-preview')
-                    .attr('src', e.target.result);
+                console.log(e.target.result);
+                // $('img.upload-preview')
+                //     .attr('src', e.target.result);
             };
 
             reader.readAsDataURL(input.files[0]);
@@ -114,19 +116,19 @@ $(document).ready(() => {
 
 
 
-    // Add upload preview in gallery admin page
+                // Add upload preview in gallery admin page
 
-//     function readURL(input) {
-//         if (input.files && input.files[0]) {
-//             const reader = new FileReader();
-//
-//             reader.onload = function(e) {
-//                 $(#preview-thumbnail)
-//                     .att('src', e.target.result)
-//             };
-//             reader.readAsDataURL(input.files[0])
-//         }
-//     }
-//
-//
-// });
+            //     function readURL(input) {
+            //         if (input.files && input.files[0]) {
+            //             const reader = new FileReader();
+            //
+            //             reader.onload = function(e) {
+            //                 $(#preview-thumbnail)
+            //                     .att('src', e.target.result)
+            //             };
+            //             reader.readAsDataURL(input.files[0])
+            //         }
+            //     }
+            //
+            //
+            // });
